065df9af4621f7703b32eb02e142d70659010803
Mason Zhong <mz9865@rit.edu>
2019-11-07 10:14:48 -0500

    Implemented parser

---
+++ src/parser.py
@@ -0,0 +1,11 @@
+import csv
+
+with open("data.csv") as data:
+    for line in data:
+        tokens = line.split(" ")
+        for i in range(len(tokens)):
+            tokens[i] = tokens[i].strip("\"")
+
+
+    
+
