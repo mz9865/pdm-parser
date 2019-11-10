7c453cb7bab1cc6afdd5241ec92e1e85e5bd4a7a
Mason Zhong <mz9865@rit.edu>
2019-11-10 18:10:25 -0500

    This shit workingg

--- src/parser.py
+++ src/parser.py
@@ -1,32 +1,68 @@
 import csv
+import codecs
 
 all_rows = []
-tokens =[]
-with open("data.csv") as data:
+tokens = []
+newRowsList = []
+
+#Open txt file and split by quotes
+with codecs.open("data.txt", "r", encoding="utf-8", errors="ignore") as data:
     for line in data:
-        tokens = line.split(" ")
-        for i in range(len(tokens)):
-            tokens[i] = tokens[i].strip("\"")
-        all_rows.append(tokens)
+        splittedLine = line.split("\"")
+        for token in splittedLine:
+            if token == "" or token == " ":
+                splittedLine.remove(token)
+            if "\n" in token:
+                token.strip("\r\n")
+        all_rows.append(splittedLine)
 
+#Strip empty spaces in the tokens
+for i in range(len(all_rows)):
+    for j in range(len(all_rows[i])):
+        if all_rows[i][j][0] == " ":
+            all_rows[i][j] = all_rows[i][j].strip(" ")
+
+#Join the two numbers for the question key
+for i in range(len(all_rows)):
+    if i > 0:
+        all_rows[i][0:2] = [' '.join(all_rows[i][0:2])]
+
+#Separate the NAs
+for row in all_rows:
+    newRow = []
+    for token in row:
+        if token[0:2] == "NA" or (token[0:2] == "1 " and "NA" in token):
+            for word in token.split(" "):
+                newRow.append(word)
+        else:
+            newRow.append(token)
+    newRowsList.append(newRow)
+
+#Remove the \r\n in the last token of the row
+for row in newRowsList:
+    if '\r\n' in row[189]:
+        row[189] = row[189].replace('\r\n', '')
+
+#Write to CSV file
 with open("output.csv", "w") as output:
-    for i in range(len(all_rows)):
-        tokens = all_rows[i]
-        string = ""
-        for token in tokens:
-            string += token
-            string += ","
-        string += "\n"
-        output.write(string)
+     for i in range(len(newRowsList)):
+         writer = csv.writer(output)
+         writer.writerow(newRowsList[i])
 
-"""        
-    for row in all_rows:
-        string = ""
-        for token in tokens:
-            string += token
-            string += ","
-        string += "\n"
-        output.write(string)
-"""
-
-
+#Fatima's approach to write CSV file
+# with open("output.csv", "w") as output:
+#     for i in range(len(all_rows)):
+#         tokens = all_rows[i]
+#         string = ""
+#         for token in tokens:
+#             string += token
+#             string += ","
+#         string += "\n"
+#         output.write(string)
+#
+#     for i in range(len(all_rows)):
+#         tokens = all_rows[i]
+#         for j in range(len(tokens)):
+#             token = tokens[j]
+#             if token[0] == "\"":
+#                 tokens.remove(token)
